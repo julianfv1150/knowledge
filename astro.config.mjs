d0187a979841a36a46fd6cf42a347599b4fcb098
unknown <julian.f.vega@gmail.com>
2024-07-05 17:08:25 -0300

    adding libs

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,9 @@
 import { defineConfig } from 'astro/config';
+import react from "@astrojs/react";
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [react(), sitemap()]
+});
